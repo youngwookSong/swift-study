0b2f7bd1742efb8f1ce2567f4c452642bcbf0fd6
youngwookSong <wook4506@naver.com>
2021-04-01 14:25:30 +0900

    다양한 방식 구현, 완료
    datePicker
    tapGesture
    delegate

--- boostcourse/login/login/ModalViewController.swift
+++ boostcourse/login/login/ModalViewController.swift
@@ -7,16 +7,31 @@
 
 import UIKit
 
-class ModalViewController: UIViewController {
+class ModalViewController: UIViewController, UIGestureRecognizerDelegate {
 
+    //tap gesture recognize 추가하면 할 수 있는 액션
+    @IBAction func taptap(_ sender: UITapGestureRecognizer) {
+        //뷰에 아무대나 탭하면 키보드 닫아줌
+        self.view.endEditing(true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        /*let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer()
+        tapGesture.delegate = self
+        
+        self.view.addGestureRecognizer(tapGesture)*/
+        //이것 델리게이트 써서 탭 제스쳐 구현 방법 -> 메소드들을 추가해 더 정교한 컨트롤 가능
     }
     
     @IBAction func dismissmodal(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    //GestureREcognizeDelegate 메소드
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        self.view.endEditing(true)
+        return true
     }
     
     /*
--- boostcourse/login/login/SecondViewController.swift
+++ boostcourse/login/login/SecondViewController.swift
@@ -9,9 +9,35 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var label: UILabel!
+    
+    let dateFormatter: DateFormatter = {
+        let formatter: DateFormatter = DateFormatter()
+        formatter.dateStyle = .medium //요건 날짜 스타일 short, medium, long 스타일이 있음
+        formatter.timeStyle = .short //요건 시간 스타일 이 코드 없으면 시간은 안나옴
+        //formatter.dateFormat = "yyyy/mm/dd hh:mm:ss" 이렇게 우리가 지정해줄 수도 있음
+        return formatter
+    }()
+    
+    @IBAction func datePickerValueChange(_ sender: UIDatePicker) {
+        //값이 바뀔때마다
+        print("value change")
+        
+        //해당하는 요소의 날짜 가져오기
+        let date:Date = sender.date
+        let dateString: String = self.dateFormatter.string(from: date)
+        
+        self.label.text = dateString
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //이건 위에 datePickerValueChanged 액션 메소드를 연결하지 않고 이렇게 사용하면 되게 하는거. 그냥 인터페이스 연결하는게 속 편할듯
+        //self.datePicker.addTarget(self, action: #selector(self.datePickerValueChange(_:)), for: UIControl.Event.valueChanged)
         // Do any additional setup after loading the view.
     }
     
