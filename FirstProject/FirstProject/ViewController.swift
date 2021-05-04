605b65a5d4c7810e425b785136105631635d6e87
youngwookSong <wook4506@naver.com>
2021-05-05 00:10:43 +0900

    ..

---
+++ FirstProject/FirstProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstProject
+//
+//  Created by 송영욱 on 2021/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ grammer1.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+let a : Int?
+var b : Int
+
+a = 3
+b = 1
+
+print(a!)
+print(b)
