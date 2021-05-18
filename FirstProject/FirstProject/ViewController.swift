69182abaf976d608ba68feff9cba11c7bc86cc33
youngwookSong <wook4506@naver.com>
2021-05-18 14:36:50 +0900

    velog

--- FirstProject/FirstProject/ViewController.swift
+++ FirstProject/FirstProject/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
