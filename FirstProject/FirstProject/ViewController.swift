1e784c18cca9ac4783cf928f10d362e1864d3eac
youngwookSong <wook4506@naver.com>
2021-05-23 01:47:07 +0900

    velog

--- FirstProject/FirstProject/ViewController.swift
+++ FirstProject/FirstProject/ViewController.swift
@@ -11,6 +11,11 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var myLabel: UILabel!
     
+    
+    @IBAction func myButtonAction(_ sender: UIButton) {
+        myLabel.text = "Hi Kim!"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
