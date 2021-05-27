7e4d308543b754995f794069c7909d8c6883d730
youngwookSong <wook4506@naver.com>
2021-05-27 18:19:17 +0900

    velog update

--- FirstProject/FirstProject/ViewController.swift
+++ FirstProject/FirstProject/ViewController.swift
@@ -16,11 +16,32 @@ class ViewController: UIViewController {
         myLabel.text = "Hi Kim!"
     }
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         myLabel.text = "Hi Song!"
-        print("HI Song!")
+        print("메모리에 View가 Load됨 (viewDidLoad)")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view가 Load됨 (viewWillAppear)")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view가 화면에 나타남 (viewDidAppear)")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view가 사라지기 전 (viewWillDisappear)")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view가 사라짐 (viewDidDisappear)")
     }
 
 
