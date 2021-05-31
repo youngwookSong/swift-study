e9345d68810d1b1adf80b0c11d3548aeeadaed04
youngwookSong <wook4506@naver.com>
2021-05-31 16:11:22 +0900

    velog update

--- FirstProject/FirstProject/ViewController.swift
+++ FirstProject/FirstProject/ViewController.swift
@@ -9,32 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var myLabel: UILabel!
+    @IBAction func segueBtn(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "segueShow", sender: self)
+    }
+    
+    @IBAction func modalPresent(_ sender: UIButton) {
+        let newVC = self.storyboard?.instantiateViewController(identifier: "modalView")
+        newVC?.modalTransitionStyle = .coverVertical
+        newVC?.modalPresentationStyle = .automatic
+        self.present(newVC!, animated: true, completion: nil)
+    }
+    
+    @IBAction func navPush(_ sender: UIButton) {
+        let pushVC = self.storyboard?.instantiateViewController(withIdentifier: "navPush")
+        self.navigationController?.pushViewController(pushVC!, animated: true)
+    }
     
     
     override func viewDidLoad() {
             super.viewDidLoad()
             print("메모리에 View가 Load됨 (viewDidLoad)")
-        }
-        
-        override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-            print("view가 Load됨 (viewWillAppear)")
-        }
-        
-        override func viewDidAppear(_ animated: Bool) {
-            super.viewDidAppear(animated)
-            print("view가 화면에 나타남 (viewDidAppear)")
-        }
-        
-        override func viewWillDisappear(_ animated: Bool) {
-            super.viewWillDisappear(animated)
-            print("view가 사라지기 전 (viewWillDisappear)")
-        }
-        
-        override func viewDidDisappear(_ animated: Bool) {
-            super.viewDidDisappear(animated)
-            print("view가 사라짐 (viewDidDisappear)")
         }
     
     
