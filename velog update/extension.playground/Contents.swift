6ee9eb362b27736a45d86c65329a80e659bed596
youngwookSong <wook4506@naver.com>
2021-07-02 15:27:32 +0900

    velog_extension

---
+++ velog update/extension.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+extension Double {
+    var squared: Double {
+        return self * self
+    }
+}
+
+let myValue:Double = 3.5
+print(myValue.squared)
+print(3.5.squared)
+print(myValue.isZero)
