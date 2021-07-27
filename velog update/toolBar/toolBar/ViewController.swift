faa1248f3fbc4db7ab3f9ea12fa32f2f0a58089c
youngwookSong <wook4506@naver.com>
2021-07-27 23:25:30 +0900

    velog update(Alert)

--- velog update/toolBar/toolBar/ViewController.swift
+++ velog update/toolBar/toolBar/ViewController.swift
@@ -11,6 +11,10 @@ class ViewController: UIViewController {
 
     @IBAction func trashItem(_ sender: Any) {
         print("Delete")
+        let sheet = UIAlertController(title: "경고", message: "정말 휴지통으로 보내겠습니까?", preferredStyle: .alert)
+        sheet.addAction(UIAlertAction(title: "Yes!", style: .destructive, handler: { _ in print("yes 클릭") }))
+        sheet.addAction(UIAlertAction(title: "No!", style: .cancel, handler: { _ in print("yes 클릭") }))
+        present(sheet, animated: true)
     }
     
     @IBAction func cameraItem(_ sender: Any) {
